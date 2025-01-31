c037a41c677df42036c3cbc8c16456ba4fd45c83
billerboom <bileomeetei@gmail.com>
2025-01-31 12:17:44 +0530

    trials

--- sketch.js
+++ sketch.js
@@ -37,7 +37,7 @@ function setup() {
 function draw() {
   translate(-video.width / 2, -video.height / 2);
   background(0);
-  image(video, 0, 0,480,720);
+  image(video, 0, 0,video.width,video.height);
 
   if (faces.length > 0) {
     let face = faces[0];
