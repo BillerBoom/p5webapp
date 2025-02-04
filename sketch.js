790c8c96bc4a22fe1652f0fe268622ed50751685
billerboom <bileomeetei@gmail.com>
2025-02-04 09:50:36 +0530

    Added button for switch and capture

--- sketch.js
+++ sketch.js
@@ -1,20 +1,20 @@
 let video;
-let faceMesh;
+let faceMesh1;
 let faces = [];
 let triangles;
 let uvCoords;
 let img;
 let x,y;
+let fcamMode = true;
+let flipButton;
+let captureButton;
+
 
 function preload() {
-  faceMesh = ml5.faceMesh({ maxFaces: 1 });
+  faceMesh1 = ml5.faceMesh({ maxFaces: 1 });
   img = loadImage("assets/grid001.png");
 }
 
-function mousePressed() {
-  console.log(faces);
-  saveCanvas('pic00','png');
-}
 
 function gotFaces(results) {
   faces = results;
@@ -22,24 +22,83 @@ function gotFaces(results) {
 
 function setup() {
   createCanvas(480,640, WEBGL);
+  flipButton=createButton("Switch");
+  flipButton.position(10,600);
+  flipButton.mousePressed(FlipCam);
+  captureButton=createButton("Capture");
+  captureButton.position(10,550);
+  captureButton.mousePressed(Capture);
   let constraints = {
     video : {
-      facingMode:"environment"
+      facingMode:"user"
     },
     audio : false
   };
   video = createCapture(constraints);
   video.hide();
-  faceMesh.detectStart(video, gotFaces);
-  triangles = faceMesh.getTriangles();
-  uvCoords = faceMesh.getUVCoords();
+  faceMesh1.detectStart(video, gotFaces);
+  triangles = faceMesh1.getTriangles();
+  uvCoords = faceMesh1.getUVCoords();
 }
+
+
+function Capture(){
+  saveCanvas('pic00','png');
+}
+
+
+async function FlipCam() {
+  stopCamera();
+  setTimeout(()=>{
+    
+    if(fcamMode){
+      startCamera("environment");
+    } else {
+      startCamera("user");
+    }
+    fcamMode=!fcamMode;
+    
+  }, 600)
+  
+  
+}
+
+
+function startCamera(cameraMode) {
+  
+    let constraints = {
+    video : { facingMode:cameraMode },
+    audio : false
+    };
+    video = createCapture(constraints);
+    video.hide(); // Hide default HTML element
+    
+    faceMesh1.detectStart(video, gotFaces);
+    triangles = faceMesh1.getTriangles();
+    uvCoords = faceMesh1.getUVCoords();
+}
+
+async function stopCamera() {
+    if (video) {
+        let stream = video.elt.srcObject;
+        if (stream) {
+            let tracks = stream.getTracks();
+            tracks.forEach(track => track.stop()); // Stop all tracks
+        }
+        video.remove(); // Remove the video element
+        video = null;
+    }
+    if (faceMesh1){
+      faceMesh1.detectStop();
+    }
+}
+
 
 function draw() {
   translate(-width / 2, -height / 2);
   background(0);
-  image(video, 0, 0,width,height);
-
+  if (video){
+  image(video, 0, 0,width,height);}
   if (faces.length > 0) {
     let face = faces[0];
     texture(img);
